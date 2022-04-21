c8d9ee4061821a6ffc7ba3dfae220f6a5eb801a2
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-04-21 15:29:00 -0400

    Opened Kunal's working files
    Created files to pull vaccination data and population data

---
+++ backend/controllers/populations.py
@@ -0,0 +1,3 @@
+# Create func to get pop data from API
+
+# Return a dict object: {country_code: # }
---
+++ backend/controllers/vaccines.py
@@ -0,0 +1,15 @@
+from typing import Dict
+import json
+
+# Get link to API
+def pull_data(link) -> Dict:
+    ...
+
+
+# Run a for loop through API
+# Find min and max for 30-day, 6-month, last year, and all time
+# Keep total count lists for 30-day, 6-month, last year, and all time
+# i.e. list[0] = {30-day, 6-month, year, all_time} -- Afghanistan
+
+def update_data(data_obj):
+    ...
