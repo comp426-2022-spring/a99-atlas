0403826a3c918f54b9d3acf54f06e433e3cb2379
Nathan Holmes <nholmes@email.unc.edu>
2022-04-22 16:45:11 -0400

    Adjust for edge cases

--- backend/controllers/deaths.py
+++ backend/controllers/deaths.py
@@ -3,30 +3,6 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 import requests, json
 
-# set up index, max and min variables, as well as dictionaries for data
-i: int = 0
-max: float = 0.0
-min: float = 0.0
-dataDict: Dict [str, float] = {} 
-returnDict: Dict = {}
-
-# most current date in data
-end_day = date.today() - relativedelta(days=1)
-start_day = end_day - relativedelta(months=6)
-
-# read in different necessary JSON files
-url = requests.get("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/internal/megafile--deaths.json")
-text = url.text
-data = json.loads(text)
-
-url2 = requests.get("https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-population.json")
-text2 = url2.text
-populations = json.loads(text2)
-
-url3 = requests.get("https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.json")
-text3 = url3.text
-codes = json.loads(text3)
-
 def getCode(name: str) -> str:
     j: int = 0
     code: str = ""
@@ -55,20 +31,70 @@ def checkValue (dpc: float):
     if dpc < min:
         min = dpc
 
-start_deaths: int = 0
-end_deaths: int = 0
+def parse(end_day: str, start_day: str) -> Dict:
+    dataDict: Dict [str, float] = {} 
+    returnDict: Dict = {}
+    i: int = 0
+    start_deaths: int = 0
+    end_deaths: int = 0
+    while i <= len(data['location']) - 1:
+        if str(data['date'][i]) == start_day:
+            start_deaths = data['total_deaths'][i]
+        if str(data['date'][i]) == end_day:
+            end_deaths = data['total_deaths'][i]
+            name = data['location'][i]
+            code = getCode(name)
+            if code != "":
+                deaths = end_deaths - start_deaths 
+                if deaths < 0:
+                    deaths = end_deaths
+                dpc = getDPC(name, deaths)
+                checkValue(dpc)
+                dataDict[code] = dpc
+        i += 1
+    returnDict['min'] = min
+    returnDict['max'] = max
+    returnDict['data'] = dataDict
+    return returnDict
 
-while i <= len(data['location']) - 1:
-    if str(data['date'][i]) == str(start_day):
-        start_deaths = data['total_deaths'][i]
-    if str(data['date'][i]) == str(end_day):
-        end_deaths = data['total_deaths'][i]
-        name = data['location'][i]
-        code = getCode(name)
-        deaths = end_deaths - start_deaths 
-        dpc = getDPC(name, deaths)
-        checkValue(dpc)
-        dataDict[code] = deaths
-    i += 1
+def main() -> Dict:
+    finalDict: Dict = {}
+    
+    end_day = date.today() - relativedelta(days=1)
+    start_day = "Beginning"
+    finalDict['allTime'] = parse(str(end_day), str(start_day));
 
-print(dataDict)
+    end_day = date.today() - relativedelta(days=1)
+    start_day = end_day - relativedelta(years=1)
+    finalDict['lastYear'] = parse(str(end_day), str(start_day));
+
+    end_day = date.today() - relativedelta(days=1)
+    start_day = end_day - relativedelta(months=6)
+    finalDict['last6Months'] = parse(str(end_day), str(start_day));
+
+    end_day = date.today() - relativedelta(days=1)
+    start_day = end_day - relativedelta(days=30)
+    finalDict['last30Days'] = parse(str(end_day), str(start_day));
+
+    #print(finalDict)
+    return finalDict
+
+if __name__ == "__main__":
+# set up max and min variables, as well as dictionaries for data
+    max: float = 0.0
+    min: float = 0.0
+
+# read in different necessary JSON files
+    url = requests.get("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/internal/megafile--deaths.json")
+    text = url.text
+    data = json.loads(text)
+
+    url2 = requests.get("https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-population.json")
+    text2 = url2.text
+    populations = json.loads(text2)
+
+    url3 = requests.get("https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.json")
+    text3 = url3.text
+    codes = json.loads(text3)
+
+    main()
