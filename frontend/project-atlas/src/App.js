c235d9ecad6aa2c381fa68633ee8bb809aa527ae
kchange7 <Kchange7@live.unc.edu>
2022-04-26 11:15:32 -0400

    changed import

--- frontend/project-atlas/src/App.js
+++ frontend/project-atlas/src/App.js
@@ -2,13 +2,11 @@ import './App.css';
 import { useState } from 'react';
 import MapChart from './components/MapChart';
 import ReactTooltip from "react-tooltip";
-import AppBar from './components/AppBar';
 
 function App() {
   const [content, setContent] = useState("");
   return (
     <div>
-      <AppBar/>
       <div className='map'><MapChart setTooltipContent={setContent} /> </div>
       <ReactTooltip>{content}</ReactTooltip>
     </div>
