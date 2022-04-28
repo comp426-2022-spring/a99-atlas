e782e3adb609a10e2d777553250417d51b62d6be
liampong <liam.pongracz@gmail.com>
2022-04-28 14:07:15 -0400

    login and register ready

--- backend/routes/userroutes.cjs
+++ backend/routes/userroutes.cjs
@@ -19,70 +19,43 @@ const { json } = require("body-parser");
 router.post('/register', (req, res) => {
     const {email, password} = req.body;
 
-        try{
-            const stmt = db.prepare(`SELECT email FROM userinfo WHERE email = ('${email}')`).all();
+    try{
+        const stmt = db.prepare(`SELECT email FROM userinfo WHERE email = ('${email}')`).all();
 
-            console.log(stmt);
-            if (stmt.length != 0) {
-                res.status(400).send("Email already in use.");
-            } else {
-                const hashedPw = getHashedPassword(password);
+        console.log(stmt);
+        if (stmt.length != 0) {
+            res.status(400).send("Email already in use.");
+        } else {
+            const hashedPw = getHashedPassword(password);
 
-                const adduser = db.prepare(`INSERT INTO userinfo (email, password) VALUES ('${email}','${hashedPw}')`);
-                const user = adduser.run();
+            const adduser = db.prepare(`INSERT INTO userinfo (email, password) VALUES ('${email}','${hashedPw}')`).run();
 
-                res.status(200).json(user);
-            }
-        } catch (e) {
-            console.error(e)
+            res.status(200).json(adduser);
         }
+    } catch (e) {
+        console.error(e)
+    }
 });
 // should take username and password in json
 const authTokens = {};
 
 router.post('/login', (req, res) => {
-    const {email, password}  =req.body;
+    const {email, password}  = req.body;
     const hashedPw = getHashedPassword(password);
 
-    console.log(db.prepare(`SELECT * FROM userinfo`).all());
-    console.log(hashedPw);
-    const user = db.prepare(`SELECT email, password FROM userinfo WHERE (email = '${email}' AND password = '${hashedPw}')`).all();
-    console.log(user)
+    const user = db.prepare(`SELECT * FROM userinfo WHERE (email = '${email}' AND password = '${hashedPw}')`).all();
 
     if (user.length > 0) {
-        const authToken = generateAuthToken();
-
-        authTokens[authToken] = user;
-        res.cookie('AuthToken', authToken);
-        res.status(200)
-
-        // res.redirect('/protected');
+        res.status(200).send("Logged in");
     } else {
         res.status(400).send("Invalid email or password");
     }
 });
 
-router.use((req, res, next) => {
-    const authToken = req.cookies['AuthToken'];
-    req.user = authTokens[authToken];
-    next();
-})
-router.get('/protected', (req, res) => {
-    if (req.user) {
-        res.status(200).send("User protected");
-    } else {
-        res.status(401).send("Please login to contiue");
-    }
-})
-
 const getHashedPassword = (password) => {
     const sha256 = crypto.createHash('sha256');
     const hash = sha256.update(password).digest('base64');
     return hash;
-}
-
-const generateAuthToken = () => {
-    return crypto.randomBytes(30).toString('hex');
 }
 
 router.get('/cases/:id', (req, res) => {
