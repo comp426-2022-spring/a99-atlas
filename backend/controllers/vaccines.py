840c36df6c6604a030cafaf4d09c984fcf3b1e62
Kunal Pathak <69764621+kunalpa@users.noreply.github.com>
2022-04-30 16:02:42 -0400

    Completely revamped vaccines.py
    Switched the database and therefore the way values were updated. First Draft

--- backend/controllers/vaccines.py
+++ backend/controllers/vaccines.py
@@ -4,83 +4,6 @@ from dateutil.relativedelta import relativedelta
 from importdata import dbimport
 import requests, json
 
-# function to get oldest date of data
-def getFDate(i: int, end_day: str) -> str:
-    fDate: str = ""
-    while str(data['date'][i]) != end_day:
-        i = i - 1
-    if str(data['date'][i]) == end_day:
-        i = i + 1
-    fDate = str(data['date'][i])
-    return fDate
-
-# function to get country code
-def getCode(name: str) -> str:
-    if name == "Bonaire Sint Eustatius and Saba":
-        name = "Bonaire, Sint Eustatius and Saba"
-    if name == "British Virgin Islands":
-        name = "Virgin Islands (British)"
-    if name == "Cape Verde":
-        name = "Cabo Verde"
-    if name == "Curacao":
-        name = "Curaçao"
-    if name == "Democratic Republic of Congo":
-        name = "Congo, Democratic Republic of the"
-    if name == "Faeroe Islands":
-        name = "Faroe Islands"
-    if name == "Iran":
-        name = "Iran (Islamic Republic of)"
-    if name == "Laos":
-        name = "Lao People's Democratic Republic"
-    if name == "Moldova":
-        name = "Moldova, Republic of"
-    if name == "Palestine":
-        name = "Palestine, State of"
-    if name == "Russia":
-        name = "Russian Federation"
-    if name == "South Korea":
-        name = "Korea, Republic of"
-    if name == "Syria":
-        name = "Syrian Arab Republic"
-    if name == "Taiwan":
-        name = "Taiwan, Province of China"
-    if name == "Tanzania":
-        name = "Tanzania, United Republic of"
-    if name == "Timor":
-        name = "Timor-Leste"
-    if name == "United Kingdom":
-        name = "United Kingdom of Great Britain and Northern Ireland"
-    if name == "United States":
-        name = "United States of America"
-    if name == "Venezuela":
-        name = "Venezuela (Bolivarian Republic of)"
-    if name == "Vietnam":
-        name = "Viet Nam"
-    if name == "Iran":
-        name = "Iran (Islamic Republic of)"
-    if name == "Bolivia":
-        name = "Bolivia (Plurinational State of)"
-    j: int = 0
-    code: str = ""
-    while j < len(codes):
-        if codes[j]['name'] == name:
-            code = codes[j]['alpha-3']
-        j += 1
-    return code
-
-# function to get Death Per Capita
-#def getDPC(name: str, deaths: int) -> float:
-#    j: int = 0
-#    dpc: float = 0
-#    population: int = 1
-#    while j < len(populations):
-#        if populations[j]['country'] == name:
-#            population = populations[j]['population']
-#        j += 1
-#    dpc = deaths / population
-#    return dpc
-
-# Function that updates min and max values
 def checkValue (vpm: float):
     global max
     global min
@@ -89,43 +12,57 @@ def checkValue (vpm: float):
     if vpm < min:
         min = vpm
 
-# function to parse through mega JSON with all vax data
-def parse(end_day: str, start_day: str) -> Dict:
-    dataList = []
-    returnDict: Dict = {}
-    i: int = 0
-    start_vacc: int = 0
-    end_vacc: int = 0
-    fDate: str =  start_day
-    while i <= len(data['location']) - 1:
-        dataDict: Dict = {} 
-        codeDict: Dict = {}
-        if str(data['date'][i]) == start_day: 
-            start_vacc = data['total_vaccinations_per_hundred'][i] # get vax at specified beginning date
-        if str(data['date'][i]) == end_day:
-            end_vacc = data['total_vaccinations_per_hundred'][i] # get vax at specified end date
-            name = data['location'][i] # get name, country code, vax in specified time, and death per capita
-            fDate = getFDate(i - 1, end_day)
-            code = getCode(name)
-            if code != "":
+
+def parse(days: int) -> Dict:
+    global date
+    return_dict: Dict = {}
+    data_list = []
+    for i in range(len(data)):
+        data_dict: Dict = {}
+        codeDict = {}
+        if len(str(data[i]['iso_code'])) == 3:
+            j = days
+            k = len(data[i]['data']) - 1
+            fDate = data[i]['data'][0]['date']
+            while True:
                 try:
-                    vax = end_vacc - start_vacc 
-                    if vax < 0:
-                        vax = end_vacc # if vax less than 0, we don't have enough data. Use total deaths instead
-                except:
-                    vax = 0
-                #dpc = getDPC(name, deaths)
-                checkValue(vax)
-                dataDict['name'] = name
-                dataDict['num'] = vax
-                dataDict['reportDate1'] = fDate
-                codeDict[code] = dataDict
-                dataList.append(codeDict)
-        i += 1 
-    returnDict['min'] = min
-    returnDict['max'] = max
-    returnDict['data'] = dataList
-    return returnDict 
+                    if k < 0:
+                        raise IndexError
+                    start_vax = data[i]['data'][k]['total_vaccinations_per_hundred']
+                    break
+                except IndexError:
+                    start_vax = 0
+                    break
+                except KeyError:
+                    k -= 1
+
+            while True:
+                try:
+                    if j < 0:
+                        raise IndexError
+                    oldvax = data[i]['data'][j]['total_vaccinations_per_hundred']
+                    break
+                except IndexError:
+                    oldvax = 0
+                    break
+                except KeyError:
+                    j -= 1
+            vax = start_vax - oldvax
+            # if days == 0:
+            #     vax = data[i]['data'][len(data[i]['data']) - 1]['total_vaccinations_per_hundred']
+            country = data[i]['country']
+            countryCode = data[i]['iso_code']
+            checkValue(vax)
+            data_dict['name'] = country
+            data_dict['num'] = vax
+            data_dict['reportDate1'] = fDate
+            codeDict[countryCode] = data_dict
+            data_list.append(codeDict)
+
+    return_dict['min'] = min
+    return_dict['max'] = max
+    return_dict['data'] = data_list
+    return return_dict
 
 def main() -> Dict:
 
@@ -133,48 +70,36 @@ def main() -> Dict:
     global min
     global max
     
-    end_day = date.today() - relativedelta(days=2)
-    start_day = "Beginning"
-    finalDict['allTime'] = parse(str(end_day), str(start_day))
+    # end_day = date.today() - relativedelta(days=2)
+    # start_day = "Beginning"
+    days = 0
+    finalDict['allTime'] = parse(days)
 
     max = 0.0 # reset min and max after every new parsing data call
     min = 100.0
 
-    end_day = date.today() - relativedelta(days=2)
-    start_day = end_day - relativedelta(years=1)
-    finalDict['lastYear'] = parse(str(end_day), str(start_day))
+    days = 365
+    finalDict['lastYear'] = parse(days)
 
     max = 0.0
     min = 100.0
 
-    end_day = date.today() - relativedelta(days=2)
-    start_day = end_day - relativedelta(months=6)
-    finalDict['last6Months'] = parse(str(end_day), str(start_day))
+    days = 183
+    finalDict['last6Months'] = parse(days)
 
     max = 0.0
     min = 100.0
 
-    end_day = date.today() - relativedelta(days=2)
-    start_day = end_day - relativedelta(days=30)
-    finalDict['last30Days'] = parse(str(end_day), str(start_day))
-
+    days = 30
+    finalDict['last30Days'] = parse(days)
+    # print(finalDict)
     return finalDict
 
 if __name__ == "__main__":
     max: float = 0.0
     min: float = 100.0 # set up max and min variables
     
-    url = requests.get("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/internal/megafile--vaccinations.json")
+    url = requests.get("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json")
     text = url.text 
     data = json.loads(text) # read in vaccine data JSON
-
-    #url2 = requests.get("https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-population.json")
-    #text2 = url2.text
-    #populations = json.loads(text2) # read in population data JSON
-
-    url3 = requests.get("https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.json")
-    text3 = url3.text
-    codes = json.loads(text3) # read in country code data JSON
-
-    finaljson = main()
-    dbimport(finaljson, "vacc")
+    main()
